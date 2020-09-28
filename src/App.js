ca61784160526fe71bfe3a4dae9bf6468dfa89aa
ikdshn <harohawayu0504@gmail.com>
2020-09-29 01:44:17 +0900

    ex10.11

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import From from "./Form";
+import List from './List'
+import Item from './Item'
+
+const App = () => {
+  return (
+    <>
+      <h1>Todo App</h1>
+      <From />
+      <List />
+      <Item />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Item.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Item = () => {
+  return (
+    <li>
+      <input type="checkbox"/>
+      <span>うんち</span>
+    </li>
+  )
+}
+
+export default Item
